1ea7a786c056d53fd98a4b497fc9a68415a6b99b
Srao734 <136876420+Srao734@users.noreply.github.com>
2023-06-24 07:53:33 +0000

    strings

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 def main():
-    ##################################################
-    # Comlete your code here
-    ##################################################
-    pass
+    sub1 = "Programming "
+    sub2 = "Python"
+    merged_str = (sub1) + (sub2)
+    print (sub1)
+    print (sub2)
+    print (merged_str)
+
+                     
+                     
+
+pass
 
 
 if __name__ == '__main__':
